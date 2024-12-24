db08632c3ffac8cf5578735dac0a4de13c45defe
Lukasz Bulinski <lucas.bulinski@gmail.com>
2024-12-24 21:01:53 +0100

    feature():tansport struct

---
+++ transport.go
@@ -0,0 +1,15 @@
+package main
+
+import "net"
+
+type Transport struct {
+	Protocol, Host, Port string
+}
+
+func (t *Transport) GetProtocol() string {
+	return t.Protocol
+}
+
+func (t *Transport) GetAddress() string {
+	return net.JoinHostPort(t.Host, t.Port)
+}
